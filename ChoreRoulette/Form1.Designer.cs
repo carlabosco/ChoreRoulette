567ea52b1090bb69860a9e6d8a466cd8f52691f7
Carla Bosco <carlabosco@gmail.com>
2018-05-22 14:15:31 -0700

    Initial commit

--- ChoreRoulette/Form1.Designer.cs
+++ ChoreRoulette/Form1.Designer.cs
@@ -28,13 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbQuestion = new System.Windows.Forms.TextBox();
+            this.tbAnswer = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbQuestion
+            // 
+            this.tbQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbQuestion.Location = new System.Drawing.Point(12, 38);
+            this.tbQuestion.Name = "tbQuestion";
+            this.tbQuestion.Size = new System.Drawing.Size(542, 30);
+            this.tbQuestion.TabIndex = 0;
+            this.tbQuestion.Text = "Who will be sharing the chores?";
+            this.tbQuestion.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tbAnswer
+            // 
+            this.tbAnswer.Location = new System.Drawing.Point(12, 74);
+            this.tbAnswer.Name = "tbAnswer";
+            this.tbAnswer.Size = new System.Drawing.Size(739, 22);
+            this.tbAnswer.TabIndex = 1;
+            this.tbAnswer.TextChanged += new System.EventHandler(this.tbAnswer_TextChanged);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Cook dinner",
+            "Do the laundry",
+            "Feed the dog",
+            "Fold the laundry",
+            "Load the dishwasher",
+            "Prepare lunchboxes",
+            "Shop for groceries",
+            "Take away the trash",
+            "Ubload dishwasher",
+            "Vacuum",
+            "Walk the dog"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 185);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(224, 208);
+            this.checkedListBox1.TabIndex = 3;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged_1);
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(12, 399);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 4;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(342, 185);
+            this.resultBox.Multiline = true;
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(224, 208);
+            this.resultBox.TabIndex = 5;
+            this.resultBox.TextChanged += new System.EventHandler(this.resultBox_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 503);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.tbAnswer);
+            this.Controls.Add(this.tbQuestion);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbQuestion;
+        private System.Windows.Forms.TextBox tbAnswer;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- ChoreRoulette/Form1.cs
+++ ChoreRoulette/Form1.cs
@@ -15,6 +15,43 @@ namespace ChoreRoulette
         public Form1()
         {
             InitializeComponent();
+
+            string answer = tbAnswer.Text;
+            string[] answerArray = answer.Split(' ');
+            int numberOfPeople = answerArray.Length;
+
+            answerArray.
+
+            foreach (string name in answerArray)
+            {
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void tbAnswer_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            string[] choresChecked = new string[] { };
+            checkedListBox1.Items.AddRange(choresChecked);
+        }
+
+        private void resultBox_TextChanged(object sender, EventArgs e)
+        {
+           
         }
     }
 }
